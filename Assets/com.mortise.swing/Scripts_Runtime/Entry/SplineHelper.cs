9dd2c58080b70ef89481c4fb2a25497ddbed772d
onovich <hjn1110@163.com>
2024-04-09 03:37:21 +0800

    <feature> add: SplineHelper.Easing2D

--- Assets/com.mortise.swing/Scripts_Runtime/Entry/SplineHelper.cs
+++ Assets/com.mortise.swing/Scripts_Runtime/Entry/SplineHelper.cs
@@ -5,7 +5,11 @@ namespace MortiseFrame.Swing {
 
     public static class SplineHelper {
 
-        public static Vector2 Easing(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float current, float duration, SplineType splineType) {
+        public static Vector2 Easing2D(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float current, float duration, SplineType splineType) {
+            return Easing(p0, p1, p2, p3, current, duration, splineType);
+        }
+
+        public static Vector3 Easing(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float current, float duration, SplineType splineType) {
             if (duration <= 0f) {
                 throw new ArgumentException("Duration Must Be Greater Than Zero.", nameof(duration));
             }
@@ -22,17 +26,17 @@ namespace MortiseFrame.Swing {
             float x = Vector4.Dot(splineMatrix * T, Px);
             float y = Vector4.Dot(splineMatrix * T, Py);
 
-            return new Vector2(x, y);
+            return new Vector3(x, y);
         }
 
-        public static float CalculateSplineLength(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, SplineType splineType, int segments = 100) {
+        public static float CalculateSplineLength(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, SplineType splineType, int segments = 100) {
             float length = 0f;
-            Vector2 prevPoint = SplineHelper.Easing(p0, p1, p2, p3, 0, 1, splineType);
-            Vector2 nextPoint;
+            Vector3 prevPoint = SplineHelper.Easing(p0, p1, p2, p3, 0, 1, splineType);
+            Vector3 nextPoint;
 
             for (int i = 1; i <= segments; i++) {
                 nextPoint = Easing(p0, p1, p2, p3, i, segments, splineType);
-                length += Vector2.Distance(prevPoint, nextPoint);
+                length += Vector3.Distance(prevPoint, nextPoint);
                 prevPoint = nextPoint;
             }
 
